bf70643498089b292a75c866eba1fbd4d77a16a2
dsjramesh7 <rameshdussa07@gmail.com>
2026-03-01 18:44:15 +0530

    modification of code

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,16 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs/server";
 import { PricingTable } from "@clerk/nextjs";
+import { Badge, Flame } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const { has } = await auth();
+  const subscriptionTier = has({ plan: "pro" } ? "pro" : "free");
   return (
     <div className="min-h-screen bg-stone-200 text-stone-500">
       <section className="pt-32 pb-20 px-4">
-        <Button variant="primary" size="xl">
-          WhatUpWEirdo
-        </Button>
+        <div>
+          <div>
+            <div>
+              <Badge
+                variant="outline"
+                className="border-2 border-orange-200 text-orange-700 bg-orange-50 text-sm font-bold mb-6 uppercase tracking-wide "
+              >
+                <Flame className="mr-1" /># 1 AI Cooking Assistant
+              </Badge>
+            </div>
+          </div>
+        </div>
       </section>
 
-      <PricingTable
+      {/* <PricingTable
         checkoutProps={{
           appearance: {
             elements: {
@@ -20,7 +33,7 @@ export default function Home() {
             },
           },
         }}
-      />
+      /> */}
     </div>
   );
 }
